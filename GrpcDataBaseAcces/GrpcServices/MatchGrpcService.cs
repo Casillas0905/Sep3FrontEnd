14eb1ac4a91c05223fa54c154f0d63781f0230f9
Casillas0905 <ikersanmartinbarreiro@gmail.com>
2023-05-25 21:25:01 +0200

    Update MatchGrpcService.cs

--- GrpcDataBaseAcces/GrpcServices/MatchGrpcService.cs
+++ GrpcDataBaseAcces/GrpcServices/MatchGrpcService.cs
@@ -1,6 +1,27 @@
-﻿namespace GrpcDataBaseAcces.GrpcServices;
+﻿using Domain.Models;
+using GrpcDataBaseAcces.IGrpcServices;
 
-public class MatchGrpcService
+namespace GrpcDataBaseAcces.GrpcServices;
+
+public class MatchGrpcService : IMatchGrpcService
 {
-    
+    public MatchDomainModel findById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void updateMatch()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void deleteMatch()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<MatchDomainModel> findByUserId(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
